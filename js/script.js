e9f17961899055767755d33daf28943ea3ca99b1
dsurdiy <dsurdiy@ukr.net>
2024-12-17 21:05:56 +0200

    added hero animations

--- js/script.js
+++ js/script.js
@@ -1,29 +1,24 @@
 "use strict";
 
-// Чекаємо поки завантажиться сторінка та усі її ресурси
 window.addEventListener("load", windowLoad);
 window.addEventListener("scroll", pageScroll);
 
 function windowLoad() {
-   // Відслідковуємо кліки на усій сторінці
    document.addEventListener("click", documentActions);
 }
 
 function documentActions(e) {
-   const targetElement = e.target; // Елемент на який клікнули
+   const targetElement = e.target;
    const bodyEl = document.body;
 
-   // Функціонал для іконки меню
    if (targetElement.closest(".menu-icon")) {
       bodyEl.classList.toggle("menu-open");
    }
 
-   // Закриваємо мобільне меню при кліках на посилання
    if (targetElement.closest(".menu__link") && bodyEl.classList.contains("menu-open")) {
       bodyEl.classList.remove("menu-open");
    }
 
-   // Плавний перехід до секцій сторінки при кліках на навігацію
    if (targetElement.hasAttribute("data-goto")) {
       e.preventDefault();
 
@@ -49,17 +44,17 @@ function pageScroll() {
 
 // -------------- Animations (IntersectionObserver) -------------------
 
-// const items = document.querySelectorAll("[data-watch]");
-// const options = {
-//    threshold: 0.2,
-// }
-// const callback = (entries) => {
-//    entries.forEach(entry => {
-//       entry.isIntersecting ? entry.target.classList.add("active") : entry.target.classList.remove("active");
-//    });
-// }
-// const observer = new IntersectionObserver(callback, options);
+const animationItems = document.querySelectorAll("[data-watch]");
+const options = {
+   threshold: 0.2,
+}
+const callback = (entries) => {
+   entries.forEach(entry => {
+      entry.isIntersecting ? entry.target.classList.add("active") : entry.target.classList.remove("active");
+   });
+}
+const observer = new IntersectionObserver(callback, options);
 
-// items.forEach(item => {
-//    observer.observe(item);
-// });
+animationItems.forEach(item => {
+   observer.observe(item);
+});
